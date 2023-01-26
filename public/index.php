ad1635ea2ee63cc2859b68fe9514bf02675f789a
a <aleksandr@lorexgrupp.ee>
2023-01-26 15:21:43 +0200

    test

---
+++ public/index.php
@@ -0,0 +1,72 @@
+<?php
+
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Autoload\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Mvc\Url as Lalala;
+use Phalcon\Db\Adapter\Pdo\Mysql;
+
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+
+$loader = new Loader();
+
+$loader->setDirectories(
+    [
+        APP_PATH . '/controllers/',
+        APP_PATH . '/models/',
+    ]
+);
+
+$loader->register();
+
+$container = new FactoryDefault();
+
+$container->set(
+    'view',
+    function () {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views/');
+        return $view;
+    }
+);
+
+$container->set(
+    'url',
+    function () {
+        $url = new Lalala();
+        $url->setBaseUri('/tutorial/');
+        return $url;
+    }
+);
+
+$container->set(
+    'db',
+    function () {
+        return new Mysql(
+            [
+                'host'     => 'localhost',
+                'username' => 'root',
+                'password' => '',
+                'dbname'   => 'tutorial',
+                'port' => '3306'
+            ]
+        );
+    }
+);
+
+$application = new Application($container);
+
+try {
+    // Handle the request
+    $url = $_SERVER['REQUEST_URI'];
+    $url = str_replace('tutorial', '', $url);
+    $url = str_replace('//', '/', $url);
+    $lala = $application->handle($url);
+
+    $lala->send();
+
+} catch (\Exception $e)  {
+    echo 'Exception: ', $e->getMessage();
+}
